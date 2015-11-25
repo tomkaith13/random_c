5d0a570ad23ac60a946e2688827205562d7a56db
root <tomkaith13@gmail.com>
2015-11-25 15:38:04 -0800

    added vector Iterators

--- interleaved_double.cpp
+++ interleaved_double.cpp
@@ -3,20 +3,55 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
 class Iterator {
-	vector<int> V;
+	vector<T> v;
+	typename vector<T>::iterator v_it;
 public:
-	Iterator(vector<int>&);
+	Iterator(vector<T>&);
+	bool hasNext();
+	T next();
 };
 
-Iterator::Iterator(vector<int> &v) {
-	V = v;
+
+template <class T>
+Iterator<T>::Iterator(vector<T> &vec) {
+	v = vec;
+	v_it = v.begin();
 }
+
+template <class T>
+bool
+Iterator<T>::hasNext() {
+
+	if (v_it != v.end())
+		return (true);
+	else
+		return (false);
+}
+
+template <class T>
+T
+Iterator<T>::next() {
+	if (hasNext()){
+		return *v_it++;
+	}
+	else
+		return(0);
+}
+
  
 int main()
 {   
- 	vector<int> v(3,5);   
- 	Iterator i(v);
+ 	vector<int> v{3,5,9,8,7,1,2,3,4};   
+ 	Iterator<int> vi(v);
+
+ 	while(vi.hasNext())
+ 		cout<<"vi data:"<<vi.next()<<endl;
+
+
+
+
     return(0);
  
 }	
